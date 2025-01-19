84995f87ff08a44a5cbf53db7f8815c4f3d919c1
Martin Kodet <martinkodet@hotmail.com>
2025-01-19 15:36:07 +0100

    Added Insurance table to the database.

--- Migrations/20250119143430_AddedInsuranceTableToDatabase.Designer.cs
+++ Migrations/20250119143430_AddedInsuranceTableToDatabase.Designer.cs
@@ -3,6 +3,7 @@ using Insurance_Final_Version.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Insurance_Final_Version.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250119143430_AddedInsuranceTableToDatabase")]
+    partial class AddedInsuranceTableToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250119143430_AddedInsuranceTableToDatabase.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Insurance_Final_Version.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedInsuranceTableToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Insurance",
+                table: "CustomerViewModel");
+
+            migrationBuilder.DropColumn(
+                name: "Insurance",
+                table: "Customer");
+
+            migrationBuilder.CreateTable(
+                name: "Insurance",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    InsuranceType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InsuranceValue = table.Column<int>(type: "int", nullable: false),
+                    CustomerViewModelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Insurance", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Insurance_CustomerViewModel_CustomerViewModelId",
+                        column: x => x.CustomerViewModelId,
+                        principalTable: "CustomerViewModel",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Insurance_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Insurance_CustomerId",
+                table: "Insurance",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Insurance_CustomerViewModelId",
+                table: "Insurance",
+                column: "CustomerViewModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Insurance");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Insurance",
+                table: "CustomerViewModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Insurance",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
