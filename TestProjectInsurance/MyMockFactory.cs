a62717476b07ea5a1f9d7f3e9436c290a745f434
Martin Kodet <martinkodet@hotmail.com>
2025-02-24 18:30:43 +0100

    Changed the folder structure, added some test methods.

---
+++ TestProjectInsurance/MyMockFactory.cs
@@ -0,0 +1,43 @@
+﻿using Insurance_Final_Version.Managers;
+using Insurance_Final_Version.Models;
+using Moq;
+
+namespace TestProjectInsurance
+{
+    public class MyMockFactory
+    {
+        private List<CustomerViewModel?> viewModels = new List<CustomerViewModel?>
+        {
+            new CustomerViewModel { Id = 0, CustomerId = 0, Name = "Martha", Surname = "Jennings", Age = 27, Email = "marthajennings@gmail.com", PhoneNumberPrefix = "+180", PhoneNumber = 123456789},
+            new CustomerViewModel { Id = 1, CustomerId = 1, Name = "Phil", Surname = "Hode", Age = 33, Email = "philhode@gmail.com", PhoneNumberPrefix = "+180", PhoneNumber = 148935482}
+        };
+
+
+        public List<CustomerViewModel?> CustomerViewModels
+        {
+            get
+            {
+                return viewModels;
+            }
+        }
+
+        public Mock<CustomerManager> GetMockCustomerManager()
+        {
+            var managerMock = new Mock<CustomerManager>();
+            managerMock
+                .Setup(m => m.GetAll())
+                .Returns(Task.FromResult(viewModels));
+            managerMock
+                .Setup(m => m.GetById(0))
+                .Returns(Task.FromResult(viewModels[0]));
+            managerMock
+                .Setup(m => m.GetById(1))
+                .Returns(Task.FromResult(viewModels[1]));
+            managerMock
+                .Setup(m => m.GetById(It.IsNotIn(0, 1)))
+                .Returns(Task.FromResult((CustomerViewModel?)null));
+
+            return managerMock;
+        }
+    }
+}
---
+++ TestProjectInsurance/TestControllers/TestCustomersController.cs
@@ -0,0 +1,147 @@
+﻿using Insurance_Final_Version.Controllers;
+using Insurance_Final_Version.Managers;
+using Insurance_Final_Version.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+
+namespace TestProjectInsurance.TestControllers
+{
+    [TestClass]
+    public class TestCustomersController
+    {
+        private MyMockFactory mockFactory;
+
+        public TestCustomersController()
+        {
+            mockFactory = new MyMockFactory();
+        }
+
+        #region Index()
+        [TestMethod]
+        public void TestIndex()
+        {
+            // Arrange
+            Mock<CustomerManager> managerMock = mockFactory.GetMockCustomerManager();
+
+            CustomersController controller = new CustomersController(managerMock.Object);
+
+            // Act
+            Task<IActionResult> allCustomersView = controller.Index();
+
+            // Assert
+            ViewResult result = (ViewResult)allCustomersView.Result;
+            List<CustomerViewModel>? resultModel = (List<CustomerViewModel>?)result.Model;
+
+            Assert.IsNotNull(resultModel);
+            Assert.AreEqual(mockFactory.CustomerViewModels, resultModel);
+        }
+
+        [TestMethod]
+        public void TestIndexWasAdded()
+        {
+            // Arrange
+            Mock<CustomerManager> managerMock = mockFactory.GetMockCustomerManager();
+
+            CustomersController controller = new CustomersController(managerMock.Object);
+
+            // Act
+            Task<IActionResult> allCustomersView = controller.Index(true, false);
+
+            // Assert
+            ViewResult viewResult = (ViewResult)allCustomersView.Result;
+            ViewDataDictionary dictionary = viewResult.ViewData;
+            Assert.IsTrue(dictionary.ContainsKey("wasAdded"));
+            Assert.IsTrue((bool)dictionary["wasAdded"]);
+            Assert.IsFalse((bool)dictionary["wasDeleted"]);
+        }
+
+        [TestMethod]
+        public void TestIndexWasDeleted()
+        {
+            // Arrange
+            Mock<CustomerManager> managerMock = mockFactory.GetMockCustomerManager();
+
+            CustomersController controller = new CustomersController(managerMock.Object);
+
+            // Act
+            Task<IActionResult> allCustomersView = controller.Index(false, true);
+
+            // Assert
+            ViewResult viewResult = (ViewResult)allCustomersView.Result;
+            ViewDataDictionary dictionary = viewResult.ViewData;
+            Assert.IsTrue(dictionary.ContainsKey("wasAdded"));
+            Assert.IsFalse((bool)dictionary["wasAdded"]);
+            Assert.IsTrue((bool)dictionary["wasDeleted"]);
+        }
+        #endregion
+
+        #region Details
+        [TestMethod]
+        public void TestDetails()
+        {
+            // Arrange
+            Mock<CustomerManager> managerMock = mockFactory.GetMockCustomerManager();
+            managerMock
+                .Setup(m => m.Mapper.Map<List<InsuranceViewModel>>(It.IsAny<List<Insurance>?>()))
+                .Returns(new List<InsuranceViewModel>());
+
+            CustomersController controller = new CustomersController(managerMock.Object);
+
+            // Act
+            Task<IActionResult> detailsView = controller.Details(0);
+
+            // Assert
+            ViewResult viewResult = (ViewResult)detailsView.Result;
+            CustomerViewModel? viewModel = (CustomerViewModel?)viewResult.Model;
+
+            Assert.IsNotNull(viewModel);
+            Assert.IsFalse((bool)viewResult.ViewData["wasEdited"]);
+            Assert.AreEqual(viewModel, mockFactory.CustomerViewModels[0]);
+        }
+
+        [TestMethod]
+        public void TestDetailsWasEdited()
+        {
+            // Arrange
+            Mock<CustomerManager> managerMock = mockFactory.GetMockCustomerManager();
+            managerMock
+                .Setup(m => m.Mapper.Map<List<InsuranceViewModel>>(It.IsAny<List<Insurance>?>()))
+                .Returns(new List<InsuranceViewModel>());
+
+            CustomersController controller = new CustomersController(managerMock.Object);
+
+            // Act
+            Task<IActionResult> detailsView = controller.Details(0, true);
+
+            // Assert
+            ViewResult viewResult = (ViewResult)detailsView.Result;
+            CustomerViewModel? viewModel = (CustomerViewModel?)viewResult.Model;
+
+            Assert.IsNotNull(viewModel);
+            Assert.IsTrue((bool)viewResult.ViewData["wasEdited"]);
+            Assert.AreEqual(viewModel, mockFactory.CustomerViewModels[0]);
+        }
+
+        [TestMethod]
+        public void TestDetailsIdNotFound()
+        {
+            // Arrange
+            Mock<CustomerManager> managerMock = mockFactory.GetMockCustomerManager();
+            managerMock
+                .Setup(m => m.Mapper.Map<List<InsuranceViewModel>>(It.IsAny<List<Insurance>?>()))
+                .Returns(new List<InsuranceViewModel>());
+
+            CustomersController controller = new CustomersController(managerMock.Object);
+
+            // Act
+            Task<IActionResult> detailsView = controller.Details(1348644982);
+            var result = detailsView.Result as NotFoundResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(404, result.StatusCode);
+        }
+        #endregion
+    }
+}
